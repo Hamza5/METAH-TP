90ddd3c7f5ea36bfa8145f66f89b6505bec5ca22
bossbux <big_boss93.19@live.com>
2016-03-25 16:07:30 +0100

    add verrification

--- src/rubikssolver/algorithms/LargeurAlgorithm.java
+++ src/rubikssolver/algorithms/LargeurAlgorithm.java
@@ -51,35 +51,44 @@ public class LargeurAlgorithm extends SolvingAlgorithm {
 	}
 
 
-    		 boolean exists(long rotations){
-    		    	long [] rotationTable= new long[8];
-    		    	int rotation = 0,nbrRotation=0;
-    		    	
-    		    	while(rotations!=0){
-    		    		rotationTable[rotation++]= rotations & 0x000F;
-    		    		rotations= rotations>>4;
-    		    		nbrRotation++;
-    		    	}
-    		    	for(int i=0;i<nbrRotation-1;i++){
-    		    		if((rotationTable[i]-1)%2==0){
-    		    			if(rotationTable[i]==(rotationTable[i+1]-1)){
-    		    				etatsabondoné++;return false;
-    		    			}
-    		    		}else{
-    		    			if(rotationTable[i]==rotationTable[i+1]+1){
-    		    				etatsabondoné++;return false;
-    		    			}
-    		    		}
-    		    		int k=i;
-    		    		while(rotationTable[k]-1==rotationTable[k+1]-1){k++;}
-    		    		if((k-i)>3){etatsabondoné++;return false;}
-    		    	}
-    		    	
-    				return false;
-    		    }
+	boolean faceMove(int M1,int M2){
+   	 if((M1==0 || M1==1) &&(M2==8 || M2==9) ) return true;
+   	 if((M1==2 || M1==3) &&(M2==4 || M2==5) ) return true;
+   	 if((M1==2 || M1==3) &&(M2==4 || M2==5) ) return true;
+      	 if((M1==6 || M1==7) &&(M2==10 || M2==11) ) return true;
+		return false;
+    }
+	 boolean exists(int rotations){
+	    	int [] rotationTable= new int[8];
+	    	int rotation = 0,nbrRotation=0;
+	    	
+	    	while(rotations!=0){
+	    		rotationTable[rotation++]= rotations & 0x000F;
+	    		rotations= rotations>>4;
+	    		nbrRotation++;
+	    	}
+	    	for(int i=0;i<nbrRotation-1;i++){
+	    		if((rotationTable[i]-1)%2==0){
+	    			if(rotationTable[i]==(rotationTable[i+1]-1)){
+	    				etatsabondoné++;return false;
+	    			}
+	    		}else{
+	    			if(rotationTable[i]==rotationTable[i+1]+1){
+	    				etatsabondoné++;return false;
+	    			}
+	    		}
+	    		int k=i;
+	    		while((rotationTable[k]-1==rotationTable[k+1]-1)||faceMove(rotationTable[k]-1,rotationTable[k+1]-1)||faceMove(rotationTable[k+1]-1,rotationTable[k]-1)){
+	    			if(!faceMove(rotationTable[k]-1,rotationTable[k+1]-1)&&!faceMove(rotationTable[k+1]-1,rotationTable[k]-1))
+	    			k++;}
+	    		if((k-i)>3){etatsabondoné++;return false;}
+	    	}
+	    	
+			return false;
+	    }
    
-    boolean ResulteState(long rotations){
-    	long [] rotationTable= new long[8];
+    boolean ResulteState(int rotations){
+    	int [] rotationTable= new int[8];
     	int rotation = 0,nbrRotation=0;
   
     	while(rotations!=0){
@@ -100,13 +109,13 @@ public class LargeurAlgorithm extends SolvingAlgorithm {
     
 	protected void doOperation() {
 	     try {
-		ArrayList<Long> BigFile= new ArrayList<>();
-		for(long i=1;i <= 12;i++ ){
+		ArrayList<Integer> BigFile= new ArrayList<>();
+		for(int i=1;i <= 12;i++ ){
 			BigFile.add(i);
 		}
 		niveau++;
 		boolean Trouvé= false;
-		long currentState = 0;
+		int currentState = 0;
 		int niveuCompteur=0;
 		int indiceNiveau=12;
 		float currentPourcentage=0;
@@ -117,7 +126,7 @@ public class LargeurAlgorithm extends SolvingAlgorithm {
 			if(!exists(currentState)){
 				if(ResulteState(currentState)){	Trouvé=true;
 				}else{	
-					long TobeAdd;
+					int TobeAdd;
 					etatsGenerés++;
 					if(niveuCompteur>indiceNiveau){
 						niveau++;
